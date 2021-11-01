5580ca71d1862e5d110c37da6c4f80021b4e8bfd
SoltamuradovTR <t.r.soltamuradov@mail.ru>
2021-11-01 14:07:43 +0300

    сделал кнопку добавления дела

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Navbar} from "./components/Navbar";
 import {TodoForm} from "./components/TodoForm";
 
 const App: React.FC = () => {
+    const [todos, setTodos] = useState([])
+
+    const addHandler = (title: string) => {
+        console.log("новое дело " + title)
+    }
+
   return (
       <>
         <Navbar/>
         <div className="container">
-          <TodoForm/>
+          <TodoForm onAdd={addHandler}/>
         </div>
       </>
   )
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,15 +1,29 @@
-import React, {useState} from "react";
+import React, {useRef} from "react";
 
-export const TodoForm: React.FC = () => {
-    const [title, setTitle] = useState("")
+interface TodoFormProps {
+    onAdd(title: string): void
+}
 
-    const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setTitle(event.target.value)
+export const TodoForm: React.FC<TodoFormProps> = ({onAdd}) => {
+    const ref = useRef<HTMLInputElement>(null)
+
+
+
+    const keyPressHandler = (event: React.KeyboardEvent) => {
+        if (event.key === "Enter") {
+            onAdd(ref.current!.value)
+            ref.current!.value = ""
+        }
     }
 
     return (
         <div className="input-field mt2">
-            <input onChange={changeHandler} value={title} type="text" id="title" placeholder="Введите название дела"/>
+            <input
+                type="text"
+                ref={ref}
+                id="title"
+                onKeyPress={keyPressHandler}
+                placeholder="Введите название дела"/>
             <label htmlFor="title" className="active">Введите название дела</label>
         </div>
     )
