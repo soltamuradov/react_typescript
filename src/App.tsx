bc0d87b05ca9a2f498a81e532237fc73b804e132
SoltamuradovTR <t.r.soltamuradov@mail.ru>
2021-11-01 15:43:03 +0300

    применил prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { Navbar } from "./components/Navbar";
-import {BrowserRouter, Switch, Route} from "react-router-dom"
-import {AboutPage} from "./components/pages/AboutPage";
-import {TodosPage} from "./components/pages/TodosPage";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { AboutPage } from "./components/pages/AboutPage";
+import { TodosPage } from "./components/pages/TodosPage";
 
 const App: React.FC = () => {
-
   return (
     <BrowserRouter>
       <Navbar />
       <div className="container">
-          <Switch>
-              <Route component={TodosPage} path="/" exact/>
-              <Route component={AboutPage} path="/about"/>
-          </Switch>
+        <Switch>
+          <Route component={TodosPage} path="/" exact />
+          <Route component={AboutPage} path="/about" />
+        </Switch>
       </div>
     </BrowserRouter>
   );
