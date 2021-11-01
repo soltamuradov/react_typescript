254bad01aa0bb6878ab2dc82d77833117dc100ed
SoltamuradovTR <t.r.soltamuradov@mail.ru>
2021-11-01 14:47:15 +0300

    сделал выполнения дела и удаление

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,28 +1,37 @@
 import React from "react";
-import {ITodo} from "./interfaces";
+import { ITodo } from "./interfaces";
 
 type TodoListProps = {
-    todos: ITodo[]
-}
+  todos: ITodo[];
+  onToggle(id: number): void;
+  onRemove(id: number): void;
+};
 
-export const TodoList: React.FC<TodoListProps> = ({todos}) => {
-    return (
-        <ul>
-            {todos.map((todo)=> {
-                const classes = ["todo"]
-                if (todo.completed) {
-                    classes.push("completed")
-                }
-                return (
-                    <li className={classes.join(" ")} key={todo.id}>
-                        <label>
-                            <input type="checkbox" checked={todo.completed}/>
-                            <span>{todo.title}</span>
-                            <i className="material-icons red-text">delete</i>
-                        </label>
-                    </li>
-                )
-            })}
-        </ul>
-    )
-}
+export const TodoList: React.FC<TodoListProps> = ({
+  todos,
+  onToggle,
+  onRemove,
+}) => {
+  if (todos.length === 0) {
+    return <p className="center">Пока дел нет!</p>
+  }
+  return (
+    <ul>
+      {todos.map((todo) => {
+        const classes = ["todo"];
+        if (todo.completed) {
+          classes.push("completed");
+        }
+        return (
+          <li className={classes.join(" ")} key={todo.id}>
+            <label>
+              <input type="checkbox" checked={todo.completed} onChange={onToggle.bind(null, todo.id)} />
+              <span>{todo.title}</span>
+              <i className="material-icons red-text" onClick={()=> onRemove(todo.id)}>delete</i>
+            </label>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
